0408fe1118f27b3c5323bf445e0e414c9af10de2
Megamind0407 <112895377+Megamind0407@users.noreply.github.com>
2024-08-29 23:34:35 +0530

    Update dateFormat

--- frontend/src/components/Form/Form.js
+++ frontend/src/components/Form/Form.js
@@ -1,16 +1,15 @@
 import React, { useState } from 'react'
-import styled from 'styled-components';
+import styled from 'styled-components'
 import DatePicker from 'react-datepicker'
-import "react-datepicker/dist/react-datepicker.css"
+import "react-datepicker/dist/react-datepicker.css";
 import { useGlobalContext } from '../../context/globalContext';
 import Button from '../Button/Button';
 import { plus } from '../../utils/Icons';
 
+
 function Form() {
-    const { addIncome } = useGlobalContext()
-
+    const {addIncome, getIncomes, error, setError} = useGlobalContext()
     const [inputState, setInputState] = useState({
-
         title: '',
         amount: '',
         date: '',
@@ -18,75 +17,87 @@ function Form() {
         description: '',
     })
 
-    const { title, amount, date, category, description } = inputState
+    const { title, amount, date, category,description } = inputState;
+
     const handleInput = name => e => {
-        setInputState({...inputState, [name]: e.target.value })
+        setInputState({...inputState, [name]: e.target.value})
+        setError('')
     }
 
     const handleSubmit = e => {
         e.preventDefault()
         addIncome(inputState)
+        setInputState({
+            title: '',
+            amount: '',
+            date: '',
+            category: '',
+            description: '',
+        })
+        getIncomes()
     }
 
     return (
         <FormStyled onSubmit={handleSubmit}>
-        <div className="input-control">
-            <input 
-                type="text" 
-                value={title}
-                name={'title'} 
-                placeholder="Salary Title"
-                onChange={handleInput('title')}
-            />
-        </div>
-        <div className="input-control">
-            <input value={amount}  
-                type="text" 
-                name={'amount'} 
-                placeholder={'Salary Amount'}
-                onChange={handleInput('amount')} 
-            />
-        </div>
-        <div className="input-control">
-            <DatePicker 
-                id='date'
-                placeholderText='Enter A Date'
-                selected={date}
-                dateFormat="dd/MM/yyyy"
-                onChange={(date) => {
-                    setInputState({...inputState, date: date})
-                }}
-            />
-        </div>
-        <div className="selects input-control">
-            <select required value={category} name="category" id="category" onChange={handleInput('category')}>
-                <option value=""  disabled >Select Option</option>
-                <option value="salary">Salary</option>
-                <option value="freelancing">Freelancing</option>
-                <option value="investments">Investments</option>
-                <option value="stocks">Stocks</option>
-                <option value="bitcoin">Bitcoin</option>
-                <option value="bank">Bank Transfer</option>  
-                <option value="youtube">Youtube</option>  
-                <option value="other">Other</option>  
-            </select>
-        </div>
-        <div className="input-control">
-            <textarea name="description" value={description} placeholder='Add A Reference' id="description" cols="30" rows="4" onChange={handleInput('description')}></textarea>
-        </div>
-        <div className="submit-btn">
-            <Button 
-                name={'Add Income'}
-                icon={plus}
-                bPad={'.8rem 1.6rem'}
-                bRad={'30px'}
-                bg={'var(--color-accent'}
-                color={'#fff'}
-            />
-        </div>
-    </FormStyled>
+            {error && <p className='error'>{error}</p>}
+            <div className="input-control">
+                <input 
+                    type="text" 
+                    value={title}
+                    name={'title'} 
+                    placeholder="Salary Title"
+                    onChange={handleInput('title')}
+                />
+            </div>
+            <div className="input-control">
+                <input value={amount}  
+                    type="text" 
+                    name={'amount'} 
+                    placeholder={'Salary Amount'}
+                    onChange={handleInput('amount')} 
+                />
+            </div>
+            <div className="input-control">
+                <DatePicker 
+                    id='date'
+                    placeholderText='Enter A Date'
+                    selected={date}
+                    dateFormat="dd/MM/yyyy"
+                    onChange={(date) => {
+                        setInputState({...inputState, date: date})
+                    }}
+                />
+            </div>
+            <div className="selects input-control">
+                <select required value={category} name="category" id="category" onChange={handleInput('category')}>
+                    <option value=""  disabled >Select Option</option>
+                    <option value="salary">Salary</option>
+                    <option value="freelancing">Freelancing</option>
+                    <option value="investments">Investiments</option>
+                    <option value="stocks">Stocks</option>
+                    <option value="bitcoin">Bitcoin</option>
+                    <option value="bank">Bank Transfer</option>  
+                    <option value="youtube">Youtube</option>  
+                    <option value="other">Other</option>  
+                </select>
+            </div>
+            <div className="input-control">
+                <textarea name="description" value={description} placeholder='Add A Reference' id="description" cols="30" rows="4" onChange={handleInput('description')}></textarea>
+            </div>
+            <div className="submit-btn">
+                <Button 
+                    name={'Add Income'}
+                    icon={plus}
+                    bPad={'.8rem 1.6rem'}
+                    bRad={'30px'}
+                    bg={'var(--color-accent'}
+                    color={'#fff'}
+                />
+            </div>
+        </FormStyled>
     )
 }
+
 
 const FormStyled = styled.form`
     display: flex;
--- frontend/src/components/Orb/Orb.js
+++ frontend/src/components/Orb/Orb.js
@@ -2,28 +2,27 @@ import React from 'react'
 import styled, { keyframes } from 'styled-components'
 import { useWindowSize } from '../../utils/useWindowSize';
 
-const Orb = () => {
+function Orb() {
 
-    const { width, height } = useWindowSize()
+    const {width, height} = useWindowSize()
+
     console.log(width, height)
 
     const moveOrb = keyframes`
-    0%{
-        transform: translate(0,0);
-    }
+        0%{
+            transform: translate(0, 0);
+        }
+        50%{
+            transform: translate(${width}px, ${height/2}px);
+        }
+        100%{
+            transform: translate(0, 0);
+        }
+    `
 
-    50%{
-        transform: translate(${width/1.2}px, ${height/2}px);
-    }
-
-    100%{
-        transform: translate(0,0);
-    }
-    
-`
     const OrbStyled = styled.div`
-        width:70vh;
-        height:70vh;
+        width: 70vh;
+        height: 70vh;
         position: absolute;
         border-radius: 50%;
         margin-left: -37vh;
@@ -35,7 +34,7 @@ const Orb = () => {
 
     return (
         <OrbStyled></OrbStyled>
-    );
-};
+    )
+}
 
 export default Orb
