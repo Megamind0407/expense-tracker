7ea8c1a5b8389ebfaa2391746035aa693c572b62
Megamind0407 <112895377+Megamind0407@users.noreply.github.com>
2024-09-06 13:49:23 +0530

    Added Fetch getIncomes

--- frontend/src/components/Incomes/Income.js
+++ frontend/src/components/Incomes/Income.js
@@ -6,11 +6,12 @@ import Form from '../Form/Form';
 import IncomeItem from '../IncomeItem/IncomeItem';
 
 function Income() {
-    const {addIncome,incomes, getIncomes, deleteIncome, totalIncome} = useGlobalContext()
+    const {incomes, getIncomes, deleteIncome, totalIncome} = useGlobalContext()
 
     useEffect(() =>{
         getIncomes()
-    }, [])
+    }, [getIncomes])
+    
     return (
         <IncomeStyled>
             <InnerLayout>
