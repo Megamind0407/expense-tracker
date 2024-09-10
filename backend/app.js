8d27e58029fbe83d39acfa2b19c53a2a7c982c5f
Megamind0407 <112895377+Megamind0407@users.noreply.github.com>
2024-09-10 14:13:18 +0530

    add new packages

--- backend/app.js
+++ backend/app.js
@@ -1,26 +1,26 @@
-const express = require('express')
-const cors = require('cors')
-const {db} = require('./db/db');
-const {readdirSync} = require('fs')
-const app = express()
+const express = require('express');
+const cors = require('cors');
+const { db } = require('./db/db'); // Database connection
+const { readdirSync } = require('fs');
+const app = express();
 
-require('dotenv').config()
+require('dotenv').config();
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 5000;
 
 //middlewares
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-//routes
-readdirSync('./routes').map((route) => app.use('/api/v1', require('./routes/' + route)))
+//routes - dynamically loading routes from the routes folder
+readdirSync('./routes').map((route) => app.use('/api/v1', require('./routes/' + route)));
 
+// Start the server and connect to the database
 const server = () => {
     app.listen(PORT, () => {
-        console.log('listening to port :', PORT)
-    })
-    db()
-}
+        console.log('Listening to port:', PORT);
+    });
+    db();
+};
 
 server();
-
