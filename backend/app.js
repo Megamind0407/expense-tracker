19a90fc6942138be20f458a3656ea341737a1868
Megamind0407 <112895377+Megamind0407@users.noreply.github.com>
2024-09-19 23:18:01 +0530

    update frontend_url

--- backend/app.js
+++ backend/app.js
@@ -8,21 +8,24 @@ require('dotenv').config();
 
 const PORT = process.env.PORT || 5000;
 
-//middlewares
+// Middlewares
 app.use(express.json());
 app.use(cors({
-    origin: 'http://localhost:3000',  // Adjust this based on your frontend URL
-    allowedHeaders: ['Authorization', 'Content-Type']
+    // Set this based on where your frontend will be deployed
+    origin: process.env.FRONTEND_URL || 'http://localhost:3000',  // Update with frontend URL for deployment
+    allowedHeaders: ['Authorization', 'Content-Type'],
 }));
 
-//routes - dynamically loading routes from the routes folder
+// Routes - Dynamically load routes from the 'routes' folder
 readdirSync('./routes').map((route) => app.use('/api/v1', require('./routes/' + route)));
 
 // Start the server and connect to the database
 const server = () => {
     app.listen(PORT, () => {
-        console.log('Listening to port:', PORT);
+        console.log(`Listening on port: ${PORT}`);
     });
+    
+    // Database connection (ensure it is connected when server starts)
     db();
 };
 
