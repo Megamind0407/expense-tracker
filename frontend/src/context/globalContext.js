477d76ff6abe4e976ea401ae91e2fc615a6aa1ad
Megamind0407 <112895377+Megamind0407@users.noreply.github.com>
2024-11-28 19:41:08 +0530

    add backend url

--- frontend/src/context/globalContext.js
+++ frontend/src/context/globalContext.js
@@ -1,7 +1,7 @@
 import React, { useContext, useState } from "react";
 import axios from 'axios';
 
-const BASE_URL = "http://localhost:5000/api/v1/";
+const BASE_URL = "https://expense-tracker-2zsi.onrender.com";
 
 const GlobalContext = React.createContext();
 
