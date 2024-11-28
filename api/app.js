d82815bdd1fa637924ad4cbd712f2f86873ca0df
Megamind0407 <112895377+Megamind0407@users.noreply.github.com>
2024-11-28 19:14:40 +0530

    add deployment

--- api/app.js
+++ api/app.js
@@ -22,7 +22,14 @@ readdirSync(routesDirectory).forEach((routeFile) => {
     const routePath = path.join(routesDirectory, routeFile);
     app.use('/api/v1', require(routePath)); // Dynamically require the route file
 });
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, 'frontend/build')));
 
+    // Catch-all route to serve the React app's index.html for all non-API routes
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
+    });
+}
 // Start the server and connect to the database
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
